88fd09c613b0748c8c895d40e7f4bd80b3ab7862
차지원 <chajiwon100785@gmail.com>
2022-01-18 14:37:59 +0900

    Issue #19 feat: add todo activity 생성

---
+++ app/src/main/java/com/gdsc/todo/ui/addtodo/AddToDoActivity.kt
@@ -0,0 +1,13 @@
+package com.gdsc.todo.ui.addtodo
+
+import android.os.Bundle
+import com.gdsc.todo.ui.BaseActivity
+import com.gdsc.todo.R
+import com.gdsc.todo.databinding.ActivityAddTodoBinding
+
+class AddToDoActivity: BaseActivity<ActivityAddTodoBinding>(R.layout.activity_add_todo) {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
