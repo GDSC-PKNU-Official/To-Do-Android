8e3554d99d8911260135950a4d01fc55a2debd90
JeongeunChoi <cju4103@gmail.com>
2022-01-23 21:29:37 +0900

    Issue #19 feat: activity, layout 구성
    git commit -m Issue

--- app/src/main/java/com/gdsc/todo/MainActivity.kt
+++ app/src/main/java/com/gdsc/todo/MainActivity.kt
@@ -1,11 +1,21 @@
 package com.gdsc.todo
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import com.gdsc.todo.databinding.ActivityAddToDoBinding
+import com.gdsc.todo.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.mainAddButton.setOnClickListener {
+            val intent = Intent(this, AddToDoActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
