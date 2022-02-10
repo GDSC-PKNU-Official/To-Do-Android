389f87b636bb753d05fb7795e6aa03dd50bd3f5e
차지원 <chajiwon100785@gmail.com>
2022-02-11 01:53:37 +0900

    Issue #36 feat: DB & DataSource 모듈 생성

---
+++ app/src/main/java/com/gdsc/todo/di/DataSourceModule.kt
@@ -0,0 +1,16 @@
+package com.gdsc.todo.di
+
+import com.gdsc.todo.data.local.ToDoLocalDataSource
+import com.gdsc.todo.data.local.ToDoLocalDataSourceImpl
+import dagger.Binds
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+
+@InstallIn(SingletonComponent::class)
+@Module
+abstract class DataSourceModule {
+
+    @Binds
+    abstract fun bindToDoDataSource(dataSource: ToDoLocalDataSourceImpl): ToDoLocalDataSource
+}
