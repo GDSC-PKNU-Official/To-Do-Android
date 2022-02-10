cd37aa287fb52c124e010b466e3b8065394c68b2
차지원 <chajiwon100785@gmail.com>
2022-02-10 13:48:01 +0900

    Issue #36 feat: home화면 FAB 동작 구현

--- app/src/main/java/com/gdsc/todo/ui/home/HomeActivity.kt
+++ app/src/main/java/com/gdsc/todo/ui/home/HomeActivity.kt
@@ -1,26 +1,44 @@
 package com.gdsc.todo.ui.home
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.viewModels
 import com.gdsc.todo.R
 import com.gdsc.todo.databinding.ActivityHomeBinding
 import com.gdsc.todo.ui.BaseActivity
 import com.gdsc.todo.ui.ToDoViewModel
+import com.gdsc.todo.ui.addtodo.AddToDoActivity
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class HomeActivity : BaseActivity<ActivityHomeBinding>(R.layout.activity_home) {
 
     private val viewModel: ToDoViewModel by viewModels()
-    private val adapter = ToDoAdapter()
+    private val adapter: ToDoAdapter by lazy { ToDoAdapter() }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding.rvHomeToDoContents.adapter = adapter
+        binding.vm = viewModel
 
-        // TODO: To Do List의 변화가 생기는지 observe
-//        viewModel.toDoList.observe(this) {
-//            adapter.submitList(this)
-//        }
+        observeToDoList()
+        observeAddFAB()
+    }
+
+    private fun observeToDoList() {
+        viewModel.toDoList.observe(this) { toDoList ->
+            adapter.submitList(toDoList)
+        }
+    }
+
+    private fun observeAddFAB() {
+        viewModel.addButtonClickEvent.observe(this) {
+            navigateAddTodo()
+        }
+    }
+
+    private fun navigateAddTodo() {
+        val intent = Intent(this, AddToDoActivity::class.java)
+        startActivity(intent)
     }
 }
