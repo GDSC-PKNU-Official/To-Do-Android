cceb9875c8b311ae8747eda15279c2f693a66e53
차지원 <chajiwon100785@gmail.com>
2022-02-10 14:58:39 +0900

    Issue #36 res: add nav component

--- app/src/main/java/com/gdsc/todo/ui/home/HomeFragment.kt
+++ app/src/main/java/com/gdsc/todo/ui/home/HomeFragment.kt
@@ -3,17 +3,20 @@ package com.gdsc.todo.ui.home
 import android.content.Intent
 import android.os.Bundle
 import androidx.activity.viewModels
+import androidx.navigation.navGraphViewModels
 import com.gdsc.todo.R
 import com.gdsc.todo.databinding.ActivityHomeBinding
+import com.gdsc.todo.databinding.FragmentHomeBinding
 import com.gdsc.todo.ui.BaseActivity
 import com.gdsc.todo.ui.ToDoViewModel
 import com.gdsc.todo.ui.addtodo.AddToDoActivity
+import com.gdsc.todo.ui.base.BaseFragment
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
-class HomeActivity : BaseActivity<ActivityHomeBinding>(R.layout.activity_home) {
+class HomeActivity : BaseFragment<FragmentHomeBinding>(R.layout.fragment_home) {
 
-    private val viewModel: ToDoViewModel by viewModels()
+    private val viewModel: ToDoViewModel by navGraphViewModels<>()
     private val adapter: ToDoAdapter by lazy { ToDoAdapter() }
 
     override fun onCreate(savedInstanceState: Bundle?) {
