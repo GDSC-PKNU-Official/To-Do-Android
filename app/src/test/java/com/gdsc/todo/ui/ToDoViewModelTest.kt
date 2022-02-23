dd493e917d645a93975e4aa4d8f26f580a89fe90
차지원 <chajiwon100785@gmail.com>
2022-02-23 18:17:52 +0900

    Issue #44 test: 완료 버튼 눌렀을 때 addToDo가 잘 실행되는지  테스트

--- app/src/test/java/com/gdsc/todo/ui/ToDoViewModelTest.kt
+++ app/src/test/java/com/gdsc/todo/ui/ToDoViewModelTest.kt
@@ -1,16 +1,20 @@
 package com.gdsc.todo.ui
 
 import com.gdsc.todo.data.entity.ToDo
-import org.junit.Assert.*
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule
 import com.gdsc.todo.data.local.ToDoLocalDataSource
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.ObsoleteCoroutinesApi
+import kotlinx.coroutines.test.*
+import org.junit.After
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
-import org.mockito.ArgumentCaptor
 import org.mockito.Mock
 import org.mockito.Mockito.*
 
+@ExperimentalCoroutinesApi
 class ToDoViewModelTest {
 
     @Rule
@@ -18,19 +22,33 @@ class ToDoViewModelTest {
     // LiveData가 Main Thread가 아니여도 set value를 할 수 있게 수정하는 테스트 룰
     val rule = InstantTaskExecutorRule()
 
+    // test용 dispatcher
+    private val testDispatcher = TestCoroutineDispatcher()
+
+    // dispatcher provider를 test용으로 구현
+    private val fakeDispatcherProviderImpl = FakeDispatcherProviderImpl(testDispatcher)
+
     @Mock
     private lateinit var mToDoLocalDataSourceImpl: ToDoLocalDataSource
 
     private lateinit var viewModel: ToDoViewModel
 
+    @ObsoleteCoroutinesApi
     @Before
     fun setUp() {
         mToDoLocalDataSourceImpl = mock(ToDoLocalDataSource::class.java)
-        viewModel = ToDoViewModel(mToDoLocalDataSourceImpl)
+        viewModel = ToDoViewModel(fakeDispatcherProviderImpl, mToDoLocalDataSourceImpl)
+
+        Dispatchers.setMain(testDispatcher)
+    }
+
+    @After
+    fun tearDown() {
+        Dispatchers.resetMain()
     }
 
     @Test
-    fun addToDoListTest_whenClickFAB_shouldAddToDoList() {
+    fun addToDoListTest_whenClickCompleteButton_shouldAddToDoList() {
         //Given title, contents
         val title = "title"
         val contents = "contents"
@@ -43,11 +61,7 @@ class ToDoViewModelTest {
         // And click FAB
         viewModel.clickCompleteButton()
 
-        // Then added To Do List
-        assertEquals(todo, ToDo(title = viewModel.title, contents = viewModel.contents))
-
-        // FIXME: NPE -> IO Thread에서 동작하는 함수이기 때문에 addToDo함수가 실행된 것을 알지 못하는 듯
-        val argumentCaptor: ArgumentCaptor<ToDo> = ArgumentCaptor.forClass(ToDo::class.java)
-//        verify(mToDoLocalDataSourceImpl).addToDo(argumentCaptor.capture())
+        // Then verify execute addToDo
+        verify(mToDoLocalDataSourceImpl).addToDo(todo)
     }
 }
