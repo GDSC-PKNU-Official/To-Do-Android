11c77b798688fa1f77b6c3153f464f7c507ad328
차지원 <chajiwon100785@gmail.com>
2022-03-13 05:01:58 +0900

    Issue #51 feat: 정렬된 todoList의 flow를 fetch하는 함수

--- app/src/main/java/com/gdsc/todo/data/local/ToDoLocalDataSourceImpl.kt
+++ app/src/main/java/com/gdsc/todo/data/local/ToDoLocalDataSourceImpl.kt
@@ -2,6 +2,7 @@ package com.gdsc.todo.data.local
 
 import androidx.lifecycle.LiveData
 import com.gdsc.todo.data.entity.ToDo
+import kotlinx.coroutines.flow.Flow
 import javax.inject.Inject
 
 class ToDoLocalDataSourceImpl @Inject constructor(
@@ -13,4 +14,6 @@ class ToDoLocalDataSourceImpl @Inject constructor(
     override fun addToDo(toDo: ToDo) {
         toDoDao.insertToDo(toDo)
     }
+
+    override suspend fun getToDoListSortedByTitle() = toDoDao.fetchAllToDoSortedByTitle()
 }
