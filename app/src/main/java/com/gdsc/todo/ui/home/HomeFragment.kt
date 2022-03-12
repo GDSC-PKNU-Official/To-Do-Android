246be4fc7e94e673c532a6d89e7c7e11f9ddda43
차지원 <chajiwon100785@gmail.com>
2022-03-13 06:00:21 +0900

    Issue #51 feat: 메뉴 클릭을 collect해 정렬된 DB를 띄우는 기능 구현

--- app/src/main/java/com/gdsc/todo/ui/home/HomeFragment.kt
+++ app/src/main/java/com/gdsc/todo/ui/home/HomeFragment.kt
@@ -1,7 +1,11 @@
 package com.gdsc.todo.ui.home
 
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
 import android.view.View
+import androidx.lifecycle.lifecycleScope
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.navGraphViewModels
 import com.gdsc.todo.R
@@ -9,6 +13,8 @@ import com.gdsc.todo.databinding.FragmentHomeBinding
 import com.gdsc.todo.ui.ToDoViewModel
 import com.gdsc.todo.ui.base.BaseFragment
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.launch
 
 @AndroidEntryPoint
 class HomeFragment : BaseFragment<FragmentHomeBinding>(R.layout.fragment_home) {
@@ -21,8 +27,13 @@ class HomeFragment : BaseFragment<FragmentHomeBinding>(R.layout.fragment_home) {
         binding.rvHomeToDoContents.adapter = adapter
         binding.vm = toDoViewModel
 
+        setHasOptionsMenu(true)
         observeToDoList()
         observeAddFAB()
+
+        viewLifecycleOwner.lifecycleScope.launch {
+            collectSortMenu()
+        }
     }
 
     private fun observeToDoList() {
@@ -40,5 +51,31 @@ class HomeFragment : BaseFragment<FragmentHomeBinding>(R.layout.fragment_home) {
     private fun navigateAddTodo() {
         val action = HomeFragmentDirections.toAddToDo()
         findNavController().navigate(action) // TODO: 부모 클래스에 선언해두자
+    }
+
+    private suspend fun collectSortMenu() {
+        toDoViewModel.sortMenuClickEvent.collect {
+            toDoViewModel.getToDoListSortedByTitle().collect { toDoList ->
+                adapter.submitList(toDoList)
+            }
+        }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
+        inflater.inflate(R.menu.sort, menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.sort -> toDoViewModel.clickSortMenu()
+            else -> throw IllegalArgumentException(ERROR_NOT_EXIST_ID)
+        }
+
+        return super.onOptionsItemSelected(item)
+    }
+
+    companion object {
+        const val ERROR_NOT_EXIST_ID = "없는 아이디"
     }
 }
