981a28dc4a2d0267d321bc04720021d0ccccbe51
차지원 <chajiwon100785@gmail.com>
2022-03-13 05:01:16 +0900

    Issue #51 feat: 정렬된 todoList의 flow를 반환하는 인터페이스 함수

--- app/src/main/java/com/gdsc/todo/data/local/ToDoLocalDataSource.kt
+++ app/src/main/java/com/gdsc/todo/data/local/ToDoLocalDataSource.kt
@@ -2,10 +2,13 @@ package com.gdsc.todo.data.local
 
 import androidx.lifecycle.LiveData
 import com.gdsc.todo.data.entity.ToDo
+import kotlinx.coroutines.flow.Flow
 
 interface ToDoLocalDataSource {
 
     fun getToDoList(): LiveData<List<ToDo>?>
 
     fun addToDo(toDo: ToDo)
+
+    suspend fun getToDoListSortedByTitle(): Flow<List<ToDo>>
 }
