57784f6de489c23201233f1501c20b97be6b9b06
차지원 <chajiwon100785@gmail.com>
2022-04-03 22:15:03 +0900

    Issue #51 feat: Add mapping todo entity

--- app/src/main/java/com/gdsc/todo/data/entity/ToDo.kt
+++ app/src/main/java/com/gdsc/todo/data/entity/ToDo.kt
@@ -2,23 +2,20 @@ package com.gdsc.todo.data.entity
 
 import android.os.Build
 import androidx.annotation.RequiresApi
-import androidx.room.Entity
-import androidx.room.PrimaryKey
 import java.time.LocalDateTime
 import java.time.ZoneOffset
 
-@Entity
 @RequiresApi(Build.VERSION_CODES.O)
-data class ToDoEntity (
-    @PrimaryKey(autoGenerate = true) val id: Long = 0,
+data class ToDo (
+    val id: Long = 0,
     val title: String,
     val contents: String,
-    val date: Long
+    val date: LocalDateTime = LocalDateTime.now()
 ) {
-    fun to() = ToDo(
+    fun to() = ToDoEntity(
         id = id,
         title = title,
         contents = contents,
-        date = LocalDateTime.ofEpochSecond(date, 0, ZoneOffset.UTC)
+        date = date.toEpochSecond(ZoneOffset.UTC)
     )
 }
